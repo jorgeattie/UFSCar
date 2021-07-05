4a4a2fc0d32b14dc0abe4daf9a6ee9087f2aafda
jorgeattie <attiejorge.22@gmail.com>
2021-07-05 14:39:14 -0300

    Add files via upload

--- one_photon_spectrum.py
+++ one_photon_spectrum.py
@@ -7,6 +7,7 @@ Created on Fri Jun 25 14:35:38 2021
 
 from qutip import *
 from scipy.fft import fft
+from scipy.fft import fftfreq
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -41,8 +42,9 @@ P = [p]*N
 R = position_vector.position_vector(d,N)
 g = delta_function.g_matrix(Gamma,R,P,k0,N)
 f = gamma_function.f_matrix(Gamma,R,P,k0,N)
-Delta = 0
 Omega = 30
+omega_sensor = 100
+Delta = -1200
 
 
 
@@ -50,49 +52,42 @@ F = N + S
 Gamma_s = Gamma
 epsilon = 10**(-4)
 
-time_points = 100
+time_points = 1000
 time_reference = 10
 taulist = np.linspace(0,time_reference,time_points)
 
-omega_points = 100
-omega_list = np.linspace(-60,60,omega_points)
-count = 0
+H_gen = hamiltonian.Hamiltonian_gen(N,R,g,Omega,Delta,k_vec,level,F)
+H_s = hamiltonian.Hamiltonian_Sensors(N,k,n_hat,R,level,F,omega_sensor,epsilon)
+H = H_gen + H_s
+L_gen = lindbladian.Lindbladian_gen(N,f,F,level)
+L_s = lindbladian.Lindbladian_Sensors(N,F,level,Gamma_s)
+c_ops = L_gen + L_s
+g1 = correlation_functions.g1_T(H,c_ops,N,k,n_hat,R,level,taulist,F)
+S = fft(g1)
+amp = np.abs(S)
 
-n1_list = []
-n_exc_1 = excited_population_n.excited_population_n_gen(N,1,F)
+freq_points = 1000
+freq_list = np.linspace(-60,60,freq_points)
 
-for omega_sensor in omega_list:
-    H_gen = hamiltonian.Hamiltonian_gen(N,R,g,Omega,Delta,k_vec,level,F)
-    H_s = hamiltonian.Hamiltonian_Sensors(N,k,n_hat,R,level,F,omega_sensor,epsilon)
-    L_gen = lindbladian.Lindbladian_gen(N,f,F,level)
-    L_s = lindbladian.Lindbladian_Sensors(N,F,level,Gamma_s)
-    H = H_gen + H_s
-    c_ops = L_gen + L_s
-    result = steadystate(H,c_ops)
-    n1 = expect(n_exc_1,result)
-    n1_list.append(n1)
-    count += 1
-    print(count)
-    
-x = omega_list
-y = n1_list
+
+x = freq_list
+y = amp
 color = 'red'
-title = 'Population'
-x_title = '$\omega_{s}$'
-y_title = '$P_{1}$'
+title = '1PS - One photon spectrum'
+x_title = '$\omega$'
+y_title = '$S$'
 scale = 'log'
-name = 'Population for 1 excitation'
-rnge = [10**(-23),10**(6)]
+name = "$N = %d \quad kd = %.2f \quad \Omega = %.1f \quad \Delta = %.1f$" %(N,k0*d,Omega,Delta)
+rnge = [10**(-4),10**(4)]
+
 plot.Plot(x,y,color,title,x_title,y_title,scale,N,k0,d,name,rnge)
 
 
 
-  
 
 
-
-
-
+    
+    
 
 
 
